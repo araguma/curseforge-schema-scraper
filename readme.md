80693c66492fe457b52b08766f3ec1ce9241714e
araguma <itetbitb@gmail.com>
2024-08-24 12:01:55 -0400

    chore: initial commit

---
+++ readme.md
@@ -0,0 +1,16 @@
+# Curseforge Schema Scraper
+
+Scrapes and converts schemas from https://docs.curseforge.com/ to TypeScript using PlayWright.
+
+## Usage
+
+Run the following commands
+
+```bash
+git clone https://github.com/araguma/curseforge-schema-scraper.git
+cd curseforge-schema-scraper/
+npm ci
+npm start
+```
+
+Once the program is done a `curseforge.ts` file will appear in the `output` directory
---
+++ src/index.ts
@@ -0,0 +1,105 @@
+import assert from 'node:assert/strict'
+import { mkdir, writeFile } from 'node:fs/promises'
+import { chromium } from 'playwright'
+
+export type Schema = {
+    name?: string
+    type?: Record<string, string>
+    enum?: string[]
+}
+
+const url = 'https://docs.curseforge.com/'
+
+async function main() {
+    const schemas: Schema[] = []
+
+    const browser = await chromium.launch()
+    const context = await browser.newContext()
+    const page = await context.newPage()
+
+    console.log(`Navigating to ${url}...`)
+    await page.goto(url)
+
+    console.log('Finding relevant elements...')
+    const elementLocators = await page
+        .locator(
+            ['h2', 'tbody', 'p']
+                .map((tag) => `${tag}:above(h1:text("CurseForge for Studios Terms of Us")):below(h1:text("Schemas"))`)
+                .join(', '),
+        )
+        .all()
+    console.log(`Found ${elementLocators.length} elements`)
+
+    console.log('Extracting inner texts...')
+    const innerTexts = await Promise.all(elementLocators.map((locator) => locator.innerText()))
+
+    console.log('Parsing inner texts...')
+    let schema: Schema = {}
+    for (const innerText of innerTexts) {
+        if (innerText === '' || innerText === 'Possible enum values:') continue
+
+        if (innerText.includes('\t')) {
+            const rows = innerText
+                .split('\n')
+                .filter((row) => {
+                    return !row.includes('=')
+                })
+                .map((row) => {
+                    const columns = row.split('\t')
+
+                    const key = columns[0]
+                    const value = columns[1]
+                    assert(key)
+                    assert(value)
+
+                    return [
+                        key.replace(/» /g, ''),
+                        value
+                            .replace(/integer\(int32\)|integer\(int64\)|integer|number\(decimal\)/g, 'number')
+                            .replace(/string\(date-time\)/g, 'string')
+                            .replace(/\[/g, '')
+                            .replace(/\]/g, '[]')
+                            .replace(/¦/g, ' | '),
+                    ]
+                })
+            schema.type = Object.fromEntries(rows)
+        } else if (innerText.includes('=')) {
+            const key = innerText.split('=')[1]
+            assert(key)
+
+            schema.enum = schema.enum ?? []
+            schema.enum.push(key)
+        } else {
+            schema = { name: innerText.replace(/-/g, '').replace(/ /g, '') }
+            schemas.push(schema)
+        }
+    }
+
+    console.log('Converting schemas to TypeScript...')
+    let output = ''
+    schemas.forEach((schema) => {
+        assert(schema.name)
+
+        if (schema.type) {
+            output += `export type ${schema.name} = {\n`
+            Object.entries(schema.type).forEach(([key, value]) => {
+                output += `    ${key}: ${value}\n`
+            })
+            output += '}\n\n'
+        } else if (schema.enum) {
+            output += `export enum ${schema.name} {\n`
+            schema.enum.forEach((key, index) => {
+                output += `    ${key} = ${index + 1},\n`
+            })
+            output += '}\n\n'
+        }
+    })
+
+    await mkdir('output', { recursive: true })
+    await writeFile('output/curseforge.ts', output)
+
+    await context.close()
+    await browser.close()
+}
+
+main().catch(console.error)
